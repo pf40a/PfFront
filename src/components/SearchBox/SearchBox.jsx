ce307cfa81abf81bec034de3930843a453129b2f
diegosierra <diegosierra@cityciudad.com>
2023-09-04 22:27:07 -0500

    carga de resultados cuando se recarga la página

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -70,6 +70,8 @@ export default function SearchBox() {
     if (getLocalStorage("search")) {
       setInputs(getLocalStorage("search"));
       validate(getLocalStorage("search"));
+
+      dispatch(searchRooms(getLocalStorage("search")));
     }
   }, [pathname]);
 
