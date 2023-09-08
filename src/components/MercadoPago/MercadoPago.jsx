945f8639cf7d5e4b8e8d5d48edc47ee8b93da510
JoseNicolas <villagrajosenicolas@gmail.com>
2023-09-08 17:12:34 -0300

    update

--- src/components/MercadoPago/MercadoPago.jsx
+++ src/components/MercadoPago/MercadoPago.jsx
@@ -91,9 +91,10 @@ const MercadoPago = ({
   const numberRooms = arrayHabitaciones.map((item) => item.numeroHabitacion);
   const habitacionesId = arrayHabitaciones.map((item) => item.id);
   const numberToString = numberRooms.join(", ");
+  const cantidadDeHabitaciones = numberRooms.length
 
   console.log(arrayHabitaciones);
-  console.log(numberToString);
+  console.log(cantidadDeHabitaciones);
   console.log(habitacionesId);
 
   const objetosSeleccionados = [];
@@ -271,10 +272,10 @@ const MercadoPago = ({
                 cantidad de niños : {niños}
               </p>
               <p className="mt-6 text-base leading-7 text-gray-600">
-                Cantidad de habitaciones :
+                Cantidad de habitaciones : {cantidadDeHabitaciones}
               </p>
               <p className="mt-6 text-base leading-7 text-gray-600">
-                Tipo de habitaciones :
+                Tipo de habitaciones :{nameToString}
               </p>
               <div className="mt-10 flex items-center gap-x-4">
                 <h4 className="flex-none text-sm font-semibold leading-6 text-indigo-600">
