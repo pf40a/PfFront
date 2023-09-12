b5f36ff6ed74dcf1247503e1a390575d8f5b6f11
JoseNicolas <villagrajosenicolas@gmail.com>
2023-09-12 16:17:09 -0300

    Rediseño

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,6 +16,7 @@ const navegacionAdmin = [
 
 const navegacionUsuario = [
   { name: "Reservas", href: "/dashboardUser", current: true },
+  { name: "Contacto", href: "/contacto", current: true },
 ];
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -175,28 +176,28 @@ const Navbar = () => {
                       <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                         <Menu.Item>
                           {({ active }) => (
-                            <a
-                              href="#"
+                            <NavLink
+                              to="/dashboardUser"
                               className={classNames(
                                 active ? "bg-gray-100" : "",
                                 "block px-4 py-2 text-sm text-gray-700"
                               )}
                             >
-                              Your Profile
-                            </a>
+                              Tu Perfil
+                            </NavLink>
                           )}
                         </Menu.Item>
                         <Menu.Item>
                           {({ active }) => (
-                            <a
-                              href="#"
+                            <NavLink
+                              to="/dashboardUser"
                               className={classNames(
                                 active ? "bg-gray-100" : "",
                                 "block px-4 py-2 text-sm text-gray-700"
                               )}
                             >
-                              Settings
-                            </a>
+                              Configuraciones
+                            </NavLink>
                           )}
                         </Menu.Item>
                         <Menu.Item>
