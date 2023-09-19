ae0b2307a588e3ac55aaa99787ae67d30edebd81
diegosierra <diegosierra@cityciudad.com>
2023-09-19 08:21:20 -0500

    bug html en reservation.jsx

--- src/components/ReservationForm/Reservation.jsx
+++ src/components/ReservationForm/Reservation.jsx
@@ -300,9 +300,10 @@ const Reservation = () => {
                 type="email"
                 onChange={(event) => handleInputChange(event, "email")}
                 autoComplete="email"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
+            
             {formErrors["email"] && (
               <p className="text-red-500">{formErrors["email"]}</p>
             )}
