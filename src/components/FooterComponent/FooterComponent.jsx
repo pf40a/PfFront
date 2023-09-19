2d51e9f60f4f5b7d8fe2dd4d09aef0bb91ecf7a4
JoseNicolas <villagrajosenicolas@gmail.com>
2023-09-19 12:12:58 -0300

    update

--- src/components/FooterComponent/FooterComponent.jsx
+++ src/components/FooterComponent/FooterComponent.jsx
@@ -4,7 +4,7 @@ const FooterComponent = () => {
   return (
     <div className="bg-white dark:bg-[#111827] py-24 sm:py-32">
     <div className="mx-auto max-w-7xl px-6 lg:px-8">
-      <h2 className="text-center text-5xl font-semibold leading-8 text-gray-900">
+      <h2 className="text-center text-5xl font-semibold leading-8 text-gray-900 dark:text-white">
         Empresas con las cuales trabajamos
       </h2>
       <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
